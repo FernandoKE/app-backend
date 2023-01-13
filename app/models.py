dd23674c7c6a3e5ed4b0be0ab4ebd6b7f539be0c
Fernando Kerum E <fernando.kerum.espinoza@gmail.com>
2023-01-13 08:59:40 -0300

    Raffle db WIP

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime as dt
 from typing import List, Optional
 
 from sqlalchemy import Column, DateTime
-from sqlmodel import Field, Relationship, SQLModel
+from sqlmodel import Field, Relationship, SQLModel, DateTime, Text
 
 
 class UserRoleLink(SQLModel, table=True):
@@ -29,6 +29,8 @@ class User(UserBase, table=True):
     roles: List["Role"] = Relationship(
         back_populates="users", link_model=UserRoleLink
     )
+    
+    raffles: List["Raffle"] = Relationship(back_populates="user")
 
 
 class UserRead(UserBase):
@@ -84,6 +86,65 @@ class APIToken(SQLModel):
 class PasswordChange(SQLModel):
     old_password: str
     new_password: str
+
+
+class RewardUserLink(SQLModel, table=True):
+    __tablename__ = "winned_rewards"
+
+    id: int = Field(primary_key=True, nullable=False, default=None)
+    #raffle_id: int = Field(primary_key=True, foreign_key="raffles.id")
+    reward_id: int = Field(primary_key=True, foreign_key="raffles_rewards.id")
+    user_id: int = Field(primary_key=True, foreign_key="users.id")
+
+
+class RaffleRewards(SQLModel, table=True):
+    __tablename__ = "raffles_rewards"
+
+    raffle_id: int = Field(primary_key=True, foreign_key="raffles.id")
+    id: int = Field(primary_key=True, default=None, nullable=False)
+    name: str = Field(max_length=32, unique=True, index=True)
+    
+    raffles: List["Raffle"] = Relationship(back_populates="rewards")
+
+class RaffleUserLink(SQLModel, table=True):
+    __tablename__ = "raffles_numbers"
+
+    id: int = Field(default=None, primary_key=True, nullable=False)
+    price: int = Field(default=0)
+    buyed_number: int = Field(default=0)
+    
+    raffle_id: int = Field(primary_key=True, foreign_key="raffles.id")
+    user_id: int = Field(primary_key=True, foreign_key="users.id")
+
+
+class RaffleBase(SQLModel):
+    title: str = Field(max_length=64, unique=True, index=True)
+    details: str = Field(max_length=256)
+    numbers: int = Field(default=0)
+
+
+class Raffle(RaffleBase, table=True):
+    __tablename__ = "raffles"
+
+    id: int = Field(default=None, primary_key=True, nullable=False)
+    state: bool = Field(default=True)
+
+    rewards: List["RaffleRewards"] = Relationship(back_populates="raffles")
+    user: List["User"] = Relationship(back_populates="raffles")
+   
+    created_by: int = Field(foreign_key="users.id")
+    #creator: "User" = Relationship(back_populates="raffles_created")
+
+
+class RaffleCreate(RaffleBase):
+    rewards: List[str] = []
+
+
+class UserRaffles(RaffleBase):
+    pass
+
+#class RaffleRead(RaffleBase):
+#    id: int
 
 
 UserRead.update_forward_refs()
